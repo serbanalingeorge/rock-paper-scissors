67e81b604ac7c6f443730e5514df336121e08973
George Serban <George_Serban@epam.com>
2018-10-19 17:48:44 +0200

    needs work

---
+++ RockPaperScissors/RockPaperScissors/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool play = true;
+
+            Player player = new Player();
+            Console.WriteLine("Type your first name: ");
+            player.firstName = Console.ReadLine();
+            Console.WriteLine("Type your last name: ");
+            player.lastName = Console.ReadLine();
+            Player.UserInfo(player.firstName, player.lastName);
+            Oponent oponent = new Oponent();
+
+            while (play)
+            {
+                Console.WriteLine("It's your turn to choose! Type rock, paper or scissors!");
+
+                string userSelection = Console.ReadLine();
+                string oponentSelection = oponent.ComputerChoice();
+
+                Console.WriteLine("The computer chose {0}.", oponentSelection);
+                Console.WriteLine(oponent.Result(userSelection, oponentSelection));
+
+                Console.WriteLine("Play Again? y/n");
+                
+                string play_again = Console.ReadLine();
+                if (play_again == "n")
+                {
+                    Console.WriteLine("Thanks for playing!");
+                    play = false;
+                }
+                else
+                {
+                    Console.WriteLine("Press ENTER to start the game again!");
+                }
+                
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Player
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+
+
+        public static void UserInfo(string firstName, string lastName)
+        {
+            Console.WriteLine("Welcome, {0} {1} to rock-paper-scissor game!", firstName, lastName);
+            Console.WriteLine("Press ENTER to start the game!");
+            Console.ReadLine();
+
+        }
+
+    }
+
+    public class Oponent
+    {
+        public string ComputerChoice()
+        {
+            string[] choices = new string[] {"rock", "paper", "scissors"};
+            Random rnd = new Random();
+            int index = rnd.Next(0, choices.Length);
+            return choices[index];
+        }
+
+        public string Result(string choice1, string choice2)
+        {
+            Dictionary<string, string> rules = new Dictionary<string, string>();
+            rules.Add("rock", "scissors");
+            rules.Add("paper", "rock");
+            rules.Add("scissors", "paper");
+
+            if (!rules.ContainsKey(choice1))
+            {
+                return "You didn't make a sensible choice!";
+            }
+            else if (rules[choice1] == choice2)
+            {
+                return "You win!";
+            }
+            else if (rules[choice2] == choice1)
+            {
+                return "You lose!";
+            }
+            else
+            {
+                return "A draw!";
+            }
+
+        }
+    }
+}
---
+++ RockPaperScissors/RockPaperScissors/Test.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Remoting.Channels;
+using System.Security.Cryptography.X509Certificates;
+using NUnit.Framework;
+
+namespace RockPaperScissors
+{
+    class Test
+    {
+        private Oponent oponent;
+        [SetUp]
+        public void Creation()
+        {
+            oponent = new Oponent();
+        }
+
+        [Test]
+        public void Returns_Win_For_Rock_And_Scissors()
+        {
+            string result = oponent.Result("rock", "scissors");
+            Assert.AreEqual("You win!", result);
+        }
+
+        [Test]
+        public void Returns_Win_For_Scissors_And_Paper()
+        {
+            string result = oponent.Result("scissors", "paper");
+            Assert.AreEqual("You win!", result);
+        }
+
+        [Test]
+        public void Returns_Win_For_Paper_And_Rock()
+        {
+            string result = oponent.Result("paper", "rock");
+            Assert.AreEqual("You win!", result);
+        }
+
+        [Test]
+        public void Returns_Lose_For_Scissors_And_Rock()
+        {
+            string result = oponent.Result("scissors", "rock");
+            Assert.AreEqual("You lose!", result);
+        }
+
+        [Test]
+        public void Returns_Lose_For_Paper_And_Scissors()
+        {
+            string result = oponent.Result("paper", "scissors");
+            Assert.AreEqual("You lose!", result);
+        }
+
+        [Test]
+        public void Returns_Lose_For_Rock_And_Paper()
+        {
+            string result = oponent.Result("rock", "paper");
+            Assert.AreEqual("You lose!", result);
+        }
+
+        [Test]
+        public void Returns_Draw_For_Rock_And_Rock()
+        {
+            string result = oponent.Result("rock", "rock");
+            Assert.AreEqual("A draw!", result);
+        }
+
+        [Test]
+        public void Returns_Draw_For_Paper_And_Paper()
+        {
+            string result = oponent.Result("paper", "paper");
+            Assert.AreEqual("A draw!", result);
+        }
+
+        [Test]
+        public void Returns_Draw_For_Scissors_And_Scissors()
+        {
+            string result = oponent.Result("scissors", "scissors");
+            Assert.AreEqual("A draw!", result);
+        }
+
+        [Test]
+        public void Returns_Message_For_Non_RPS_Choise()
+        {
+            string result = oponent.Result("", "");
+            Assert.AreEqual("You didn't make a sensible choice!", result);
+        }
+    }
+}
